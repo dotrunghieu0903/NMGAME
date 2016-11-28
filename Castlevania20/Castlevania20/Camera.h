8434ef0abba635b09afeb97d9800a01932baa352
Hieu.Do <dotrunghieu0903@gmail.com>
2016-11-29 03:41:33 +0700

    update camera

--- Castlevania20/Castlevania20/Camera.h
+++ Castlevania20/Castlevania20/Camera.h
@@ -1,20 +1,18 @@
 #ifndef _CAMERA_H_
 #define _CAMERA_H_
 #include "Globals.h"
-#include <fstream>
 
-using namespace std;
 class Camera
 {
 public:
 	Camera();
-
-	void UpdateCamera(D3DXVECTOR3* cameraLocation);
+	void UpdateCamera(D3DXVECTOR2 cameraLocation);
 	~Camera();
 private:
-	static Camera* objCamera; //khai báo biến tĩnh khởi tạo 1 lần dùng cả chương trình.
-	D3DXVECTOR2 m_previousPosition;
+	float m_X;
+	float m_Y;
+	float m_lastY;
+	D3DXMATRIX m_Matrix;
+	RECT m_Rect;
 };
-
-
 #endif
