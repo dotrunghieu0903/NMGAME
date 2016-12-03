7ca2166a79693c7d6a8ce96f47cae1e3a4f98bcc
Minh.Tran <minh.tran@zinimedia.com>
2016-12-04 00:37:41 +0700

    state version 0.1

---
+++ Castlevania20/Castlevania20/FrameWork.h
@@ -0,0 +1,19 @@
+#ifndef _FRAMEWORK_
+#define _FRAMEWORK_
+
+#include "Globals.h"
+#include "Sprite.h"
+//#include "CSurface.h"
+//#include "CAudio.h"
+#include "Input.h"
+#include "Rect.h"
+#include "ResourceManager.h"
+//#include "SoundManager.h"
+//#include "CCamera.h"
+#include "Text.h"
+#include <map>
+#include <string>
+#include <vector>
+#include <list>
+using namespace std;
+#endif
---
+++ Castlevania20/Castlevania20/GameState.h
@@ -0,0 +1,13 @@
+#ifndef _GAMESTATE_H
+#define _GAMESTATE_H
+
+//#include"MainGameScreen.h"
+#include"MenuScreen.h"
+//#include"PauseScreen.h"
+#include "ResourceManager.h"
+#include"StateManager.h"
+//#include "OptionScreen.h"
+//#include "FinishLevelScreenk.h"
+//#include "HelpScreen.h"
+
+#endif
