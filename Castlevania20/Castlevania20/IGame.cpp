049e995595976220483caa3f6dac18b0dd2379c1
Minh.Tran <minh.tran@zinimedia.com>
2016-12-03 15:21:45 +0700

    update iGame

--- Castlevania20/Castlevania20/IGame.cpp
+++ Castlevania20/Castlevania20/IGame.cpp
@@ -1,4 +1,4 @@
-#include "IGame.h"
+﻿#include "IGame.h"
 
 IGame::IGame(HINSTANCE hInstance, int _mode, bool _Isfullscreen, int _frameRate)
 {
@@ -44,30 +44,33 @@ void IGame::SetScreenDimension()
 
 LRESULT CALLBACK IGame::WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
-	switch (msg)
+	// Kiểm tra xem có thông điệp nào được gửi tới hàng đợi cửa ứng dụng không 
+	switch (msg)				//lọc các thông điệp 
 	{
-	case WM_DESTROY:
-		PostQuitMessage(0);
+	case WM_DESTROY:			//bắt thông điệp yêu cầu kết thúc ứng dụng 
+		PostQuitMessage(0);		//gọi hàm xử lý 
 		break;
-	default:
-		return DefWindowProc(hwnd, msg, wParam, lParam);
 	}
+	// Thông điệp trả về cho quá trình
+	return DefWindowProc(hwnd, msg, wParam, lParam);
 }
 
 bool IGame::InitWindow()
 {
 	// Register to windows
 	WNDCLASS wndclass;
-	wndclass.cbClsExtra = 0;
-	wndclass.cbWndExtra = 0;
-	wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
-	wndclass.hCursor = LoadCursor(NULL, IDI_APPLICATION);
-	wndclass.hIcon = LoadIcon(NULL, IDC_ARROW);
-	wndclass.hInstance = m_hInstance;
-	wndclass.lpfnWndProc = WndProc;
-	wndclass.lpszClassName = GL_AppName;
-	wndclass.lpszMenuName = NULL;
-	wndclass.style = CS_HREDRAW | CS_VREDRAW;
+	//wndclass.cbSize = sizeof(WNDCLASSEX);							// hàm sizeof() trả về kích thước của một đối tượng kiểu dữ liệu đầu vào – đơn vị tính là byte   
+	//wndclass.hIconSm = 0;											// con trỏ tới dữ liệu biểu tượng cửa sổ ứng dụng 
+	wndclass.cbClsExtra = 0;										// xác lập số byte cấp phát thêm cho Class   
+	wndclass.cbWndExtra = 0;										// xác lập số byte cấp phát thêm cho mỗi instance của Class 
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);	// màu nền của cửa sổ   
+	wndclass.hCursor = LoadCursor(NULL, IDI_APPLICATION);			// xác lập kiểu con trỏ chuột mặc định   
+	wndclass.hIcon = LoadIcon(NULL, IDC_ARROW);						// loại biểu tượng chương trình   
+	wndclass.hInstance = m_hInstance;								// con trỏ (handle) trỏ tới instance của ứng dụng   
+	wndclass.lpfnWndProc = WndProc;									// xác lập tên hàm gọi lại callback procedure  
+	wndclass.lpszClassName = GL_AppName;							// tên lớp đăng ký với hệ thống   
+	wndclass.lpszMenuName = NULL;									// con trỏ trỏ tới object dữ liệu thực đơn ứng dụng   
+	wndclass.style = CS_HREDRAW | CS_VREDRAW;						// xác lập kiểu lớp   
 
 	if (!RegisterClass(&wndclass))
 	{
@@ -75,26 +78,20 @@ bool IGame::InitWindow()
 		return false;
 	}
 	DWORD Style;
-	Style = GL_windowMode ? WS_EX_TOPMOST | WS_POPUP | WS_VISIBLE : WS_OVERLAPPEDWINDOW;
-	if (GL_windowMode == true)
-	{
-		Style = WS_EX_TOPMOST | WS_VISIBLE | WS_POPUP;
-	}
-	else
-	{
-		Style = WS_VISIBLE | WS_SYSMENU | WS_MINIMIZEBOX | WS_DLGFRAME; //WS_OVERLAPPEDWINDOW;
-	}
-	m_hWindow = CreateWindow(GL_AppName,
-		GL_AppName,
-		Style,
-		CW_USEDEFAULT,
-		CW_USEDEFAULT,
-		GL_scrWidth,
-		GL_scrHeight,
-		NULL,
-		NULL,
-		m_hInstance,
-		this
+	//Style = GL_windowMode ? WS_EX_TOPMOST | WS_POPUP | WS_VISIBLE : WS_OVERLAPPEDWINDOW;
+	Style = GL_windowMode ? WS_EX_TOPMOST | WS_VISIBLE | WS_POPUP : WS_VISIBLE | WS_SYSMENU | WS_MINIMIZEBOX | WS_DLGFRAME;
+	m_hWindow = CreateWindow(
+		GL_AppName,			// tên lớp sử dụng đã khai báo và đăng ký
+		GL_AppName,			// tiêu đề của cửa sổ chương trình
+		Style,				// loại cửa sổ chương trình
+		CW_USEDEFAULT,		// toạ độ X của của sổ khi xuất hiện
+		CW_USEDEFAULT,		// toạ độ Y của của sổ khi xuất hiện 
+		GL_scrWidth,		// kích thước bề ngang của cửa sổ - đơn vị là pixel
+		GL_scrHeight,		// kích thước chiều cao của cửa sổ 
+		NULL,				// con trỏ trỏ tới đối tượng cha ;  NULL = đối tượng quản lý là desktop của Windows 
+		NULL,				// con trỏ đối tượng menu của chương trình; NULL = không sử dụng   
+		m_hInstance,		// con trỏ instance của ứng dụng 
+		this				// giá trị được truyền cho cửa sổ 
 	);
 	if (!m_hWindow)
 	{
@@ -111,11 +108,11 @@ bool IGame::GameInit()
 {
 	if (!InitWindow())
 		return false;
-	//GL_graphic = new CGraphic(m_hWindow, m_hInstance, GL_scrWidth, GL_scrHeight, GL_windowMode);
+	GL_graphic = new Graphic(m_hWindow, m_hInstance, GL_scrWidth, GL_scrHeight, GL_windowMode);
 	//GL_timer = new	CTimer();
-	/*if (!GL_graphic->Init())
+	if (!GL_graphic->Init())
 	return false;
-	m_Input = new CInput();*/
+	//m_Input = new CInput();
 	/*if (!m_Input->CreateInput())
 	return false;
 	if (!m_Input->InitKeyboard(m_hWindow))
@@ -160,7 +157,7 @@ void IGame::GameRun()
 void IGame::GameEnd()
 {
 	Destroy();
-	//GL_graphic->~CGraphic();
+	GL_graphic->~Graphic();
 	//m_Input->~CInput();
 	if (m_hWindow)
 	{
