d631a0f605b1166f06a02cb3ac4dc4b819c97fc0
Hieu.Do <dotrunghieu0903@gmail.com>
2016-12-22 00:34:28 +0700

    update Board

--- framework/Castlevania20/Castlevania20/Board.cpp
+++ framework/Castlevania20/Castlevania20/Board.cpp
@@ -1,14 +1,31 @@
 #include "Board.h"
 
-
-
 Board::Board()
 {
-	_sprite = new Sprite(new Texture(L"resource\\images\\board.bmp"), 10);
+	_sprite = new Sprite(new Texture(BOARD_PATH), 10);
+	_score = 0;
+	_time = 300;
+	_state = 1;
+	_life = 3;
+	_heart = 5;
+	_state = 1;
+	_simonHP = 16;
+	_itemHP = 16;
 }
 
 void Board::Draw(int x, int y) {
-	_sprite->Draw(x,y);
+	_sprite->Draw(x,y);//render borad
+	//render score
+		
+}
+
+void Board::Update(int deltaT) {
+	//Update score
+	int c = _score;
+	for (int i = 6; i > 0; i--) {
+		int k = c % 10;
+
+	}
 }
 
 Board::~Board()
--- framework/Castlevania20/Castlevania20/Board.h
+++ framework/Castlevania20/Castlevania20/Board.h
@@ -1,11 +1,25 @@
-#pragma once
+#ifndef _BOARD_H_
+#define _BOARD_H_
+
 #include "Sprite.h"
+#define BOARD_PATH L"resource\\images\\board.bmp"
 class Board
 {
+protected:
+	int _time;
+	int _state;
+	int _heart;
+	int _life;
+	int _simonHP;
+	int _itemHP;
+
 	Sprite *_sprite;
 public:
+	int _score;
 	Board();
-	void Draw(int x,int y);
+	void Draw(int x, int y);
+	void Update(int deltaTime);
 	~Board();
 };
 
+#endif // !_BOARD_H_
