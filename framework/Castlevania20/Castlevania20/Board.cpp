e069d5c8d4aa263a6f47957e5bc1e8051620adbf
Hieu Do <do.trung.hieu@inte.co.jp>
2016-12-22 16:01:36 +0700

    update board

--- framework/Castlevania20/Castlevania20/Board.cpp
+++ framework/Castlevania20/Castlevania20/Board.cpp
@@ -13,10 +13,52 @@ Board::Board()
 	_itemHP = 16;
 }
 
+void Board::DrawPoint(D3DXVECTOR3 point, int num){
+	float _cameraX = Camera::getCurrentCamera()->getViewPortX();
+	float _cameraY = Camera::getCurrentCamera()->getViewPortY();
+	point.x = _cameraX;
+	point.y = _cameraY;
+	switch (num)
+	{
+	case 1: 
+		_temp = _rect1;
+		break;
+	case 2:
+		_temp = _rect2;
+		break;
+	case 3:
+		_temp = _rect3;
+		break;
+	case 4:
+		_temp = _rect4;
+		break;
+	case 5:
+		_temp = _rect5;
+		break;
+	case 6:
+		_temp = _rect6;
+		break;
+	case 7:
+		_temp = _rect7;
+		break;
+	case 8:
+		_temp = _rect8;
+		break;
+	case 9:
+		_temp = _rect9;
+		break;
+	case 10:
+		_temp = _rect10;
+		break;
+	default:
+		break;
+	}
+}
+
 void Board::Draw(int x, int y) {
 	_sprite->Draw(x,y);//render borad
 	//render score
-		
+	_sprite->DrawRect(x,y,_temp);
 }
 
 void Board::Update(int deltaT) {
--- framework/Castlevania20/Castlevania20/Board.h
+++ framework/Castlevania20/Castlevania20/Board.h
@@ -2,6 +2,7 @@
 #define _BOARD_H_
 
 #include "Sprite.h"
+#include "Camera.h"
 #define BOARD_PATH L"resource\\images\\board.bmp"
 class Board
 {
@@ -14,11 +15,24 @@ protected:
 	int _itemHP;
 
 	Sprite *_sprite;
+	RECT _temp;
+	RECT _rect1;
+	RECT _rect2;
+	RECT _rect3;
+	RECT _rect4;
+	RECT _rect5;
+	RECT _rect6;
+	RECT _rect7;
+	RECT _rect8;
+	RECT _rect9;
+	RECT _rect10;
+
 public:
 	int _score;
 	Board();
 	void Draw(int x, int y);
 	void Update(int deltaTime);
+	void DrawPoint(D3DXVECTOR3 point, int num);
 	~Board();
 };
 
