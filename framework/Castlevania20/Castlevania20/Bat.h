6e93143ab10bd7cc968b797810f6701b1f8914b2
Hieu.Do <dotrunghieu0903@gmail.com>
2016-12-26 02:16:53 +0700

    add bat

---
+++ framework/Castlevania20/Castlevania20/Bat.h
@@ -0,0 +1,28 @@
+﻿#ifndef _BAT_H_
+#define _BAT_H_
+
+#include "BaseObject.h"
+#define BAT_SPRITE L"resource\sprite\bat.png"
+#define TYPE TypeGame::Enemy_Bat
+#define _batWIDTH	32
+#define _batHEIGHT	32
+
+enum BATSTATE
+{
+	isSleep = 1,
+	isAwake = 2,
+};
+
+class Bat: public BaseObject
+{
+private: 
+	Box _awakeBox;
+	bool _isSleep;// đang trên tường
+public:
+	Bat();
+	Bat(int id, int x,int y);
+	void Moveupdate();
+	void Update();
+	~Bat();
+};
+#endif
